c291621527084207207ac890c62256046cb243b4
Kunal Kotkar <kunalkotkar28@gmail.com>
2023-07-24 14:42:06 -0400

    added new split matlab code

--- DataPreprocessing/savepng.py
+++ DataPreprocessing/savepng.py
@@ -2,23 +2,29 @@ import os
 import pydicom
 from PIL import Image
 
-dicom_dir = 'C:/Users/kkotkar1/Desktop/CropAllDicoms/YoloData/AllDicom'
 
-png_dir = 'C:/Users/kkotkar1/Desktop/CropAllDicoms/YoloData/AllDicomPNG'
+if __name__ == '__main__':
 
-dicom_files = [f for f in os.listdir(dicom_dir) if f.endswith('.dcm')]
+    ippath = 'C:/Users/kunal/Desktop/This PC/HEPIUS/Pre Injury Sagittal B Mode/frames'
 
-for i,dicom_file in enumerate(dicom_files):
-    dicom_path = os.path.join(dicom_dir, dicom_file)
-    dicom_data = pydicom.read_file(dicom_path)
+    oppath = 'C:/Users/kunal/Desktop/This PC/HEPIUS/Pre Injury Sagittal B Mode/png'
 
-    dicom_image = dicom_data.pixel_array
-    image = Image.fromarray(dicom_image)
+    dicom_files = [f for f in os.listdir(ippath) if f.endswith('.dcm')]
 
-    png_file = os.path.splitext(dicom_file)[0] + '.png'
-    png_path = os.path.join(png_dir, png_file)
-    image.save(png_path, format='png')
-    if (i%1000 == 0):
-        print(f'Image {i} converted')
+    os.makedirs(oppath, exist_ok=True)
 
-print('Done')
+    for i,dicom_file in enumerate(dicom_files):
+        dicom_path = os.path.join(ippath, dicom_file)
+        dicom_data = pydicom.read_file(dicom_path)
+
+        dicom_image = dicom_data.pixel_array
+        image = Image.fromarray(dicom_image)
+
+        png_file = os.path.splitext(dicom_file)[0] + '.png'
+        png_path = os.path.join(oppath, png_file)
+        
+        image.convert('L').save(png_path, format='png')
+        if (i%1000 == 0):
+            print(f'Image {i} converted')
+
+    print('Done')
